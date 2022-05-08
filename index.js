a67ddac7dca27b961cd747bd815b2bef058d33e1
chaitanya <sparkscj234@gmail.com>
2022-05-08 11:31:08 +0530

    base apparel

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ const error_message = document.querySelector('.error-message');
 //(yourname) @ (domain) . (extension)(.again) 
 //sparkscj23131 @ gmail . com
 //paper @ company . ac .in
-form.addEventListener('submit',e => {
+form.addEventListener('submit', e => {
     e.preventDefault();
-    const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-    const userinput = document.querySelector('input');
-    if (re.test(userinput.value))
+    const re = /^([a-zA-Z0-9\.!$%&'*+/=?^_`{|}~-]+)@([a-zA-Z0-9-]+)(?:\.[a-zA-Z0-9-]+)*$/
+    // const re = /^([a-zA-Z0-9\.-_]+)@([a-zA-Z\d]+)\.([a-zA-Z]{2,8})(?:\.[a-zA-Z]{2,8})$/
+    const userinput = document.querySelector('input').value.trim();
+    if (re.test(String(userinput)))
     {
         form.classList.remove('error-border');
         error_image.classList.add('hide');
         error_message.classList.add('hide');
+        console.log('matched')
     }
-    else{
+
+    else {
         form.classList.add('error-border');
         error_image.classList.remove('hide');
         error_message.classList.remove('hide');
